ebeeb6dd47fac075a66da0381c440ef9d1319d00
Luka Silic <silicluka520@gmail.com>
2023-04-05 10:59:55 -0400

    nightmare nightmare nightmare nightmare

--- Lists Assignment/Program.cs
+++ Lists Assignment/Program.cs
@@ -27,7 +27,7 @@
             Random generator = new Random();
             string optionSelect;
             bool validRemoveInt = false, validAddInt = false, validFindInt = false, done = false;
-            int randIndex, occ10 = 0, occ11 = 0, occ12 = 0, occ13 = 0, occ14 = 0, occ15 = 0, occ16 = 0, occ17 = 0, occ18 = 0, occ19 = 0, occ20 = 0, king;
+            int randIndex;
             for (int i = 0; i < 25; i++)    
             {
                 randInts.Add(generator.Next(10, 21));
@@ -65,11 +65,12 @@
                         Console.Write(randInts[i] + ", ");
                     }
                 }
+
                 else if (optionSelect.ToUpper() == "GENERATE NEW LIST" || optionSelect == "2")
                 {
                     randInts.Clear();
                     Console.WriteLine("New List Generated:");
-                    for (int i = 0; i < 25; i++)
+                    for (int i = 0; i < randInts.Count; i++)
                     {
                         randInts.Add(generator.Next(10, 21));
                         if (i != 24)
@@ -82,6 +83,7 @@
                         }
                     }
                 }
+
                 else if (optionSelect.ToUpper() == "REMOVE VALUE" || optionSelect == "3")
                 {
                     Console.WriteLine("Please specify the value you wish to remove:");
@@ -107,6 +109,7 @@
                         Console.Write(randInts[i] + ", ");
                     }
                 }
+
                 else if (optionSelect.ToUpper() == "ADD VALUE" || optionSelect == "4")
                 {
                     Console.WriteLine("Please enter the value you wish to enter:");
@@ -128,11 +131,12 @@
                     }
                     randIndex = generator.Next(1, 26);
                     randInts.Insert(randIndex, addInt);
-                    for (int i = 0; i < 25; i++)
+                    for (int i = 0; i < randInts.Count; i++)
                     {
                         Console.Write($"{randInts[i]}, ");
                     }
                 }
+
                 else if (optionSelect.ToUpper() == "COUNT OCCURRENCES" || optionSelect == "5")
                 {
                     Console.WriteLine("Please enter the value you wish to count:");
@@ -160,139 +164,177 @@
                     }
                     Console.WriteLine($"List contains {countOccInt} {occurrances} time(s)");
                 }
+
                 else if (optionSelect.ToUpper() == "FIND GREATEST" || optionSelect == "6")
                 {
                     Console.WriteLine($"The maximum value in the list is {randInts.Max()}");
                 }
+
                 else if (optionSelect.ToUpper() == "FIND LEAST" || optionSelect == "7")
                 {
                     Console.WriteLine($"The maximum value in the list is {randInts.Min()}");
                 }
+
                 else if (optionSelect.ToUpper() == "CALCULATE SUM AND AVERAGE" || optionSelect == "8")
                 {
                     Console.WriteLine($"The sum of all list index values is {randInts.Sum()}");
                     Console.WriteLine($"The average index value is {randInts.Average()}");
                 }
+
                 else if (optionSelect.ToUpper() == "FIND MOST FREQUENTLY OCCURRING" || optionSelect == "9")
                 {
-                    for (int i = 10; i <= 20; i++)
-                    {
-                        for (int j = 0; j <= 25; j++)
-                        {
-                            if (randInts[j] == i)
-                            {
-                                if (i == 10)
-                                {
-                                    occ10++;
-                                }
-                                else if (i == 11)
-                                {
-                                    occ11++;
-                                }
-                                else if (i == 12)
-                                {
-                                    occ12++;
-                                }
-                                else if (i == 13)
-                                {
-                                    occ13++;
-                                }
-                                else if (i == 14)
-                                {
-                                    occ14++;
-                                }
-                                else if (i == 15)
-                                {
-                                    occ15++;
-                                }
-                                else if (i == 16)
-                                {
-                                    occ16++;
-                                }
-                                else if (i == 17)
-                                {
-                                    occ17++;
-                                }
-                                else if (i == 18)
-                                {
-                                    occ18++;
-                                }
-                                else if (i == 19)
-                                {
-                                    occ19++;
-                                }
-                                else if (i == 20)
-                                {
-                                    occ20++;
-                                }
-                            }
-                        }
-                    }
-                    king = 10;
-                    if (occ11 > occ10)
-                    {
-                        king = 11;
-                    }
-                    if (occ12 > occ10)
-                    {
-                        king = 12;
-                    }
-                    if (occ13 > occ10)
-                    {
-                        king = 13;
-                    }
-                    if (occ14 > occ10)
-                    {
-                        king = 14;
-                    }
-                    if (occ15 > occ10)
-                    {
-                        king = 15;
-                    }
-                    if (occ16 > occ10)
-                    {
-                        king = 16;
-                    }
-                    if (occ17 > occ10)
-                    {
-                        king = 17;
-                    }
-                    if (occ18 > occ10)
-                    {
-                        king = 18;
-                    }
-                    if (occ19 > occ10)
-                    {
-                        king = 19;
-                    }
-                    if (occ20 > occ10)
-                    {
-                        king = 20;
-                    }
-                    instances.Add(occ10);
-                    instances.Add(occ11);
-                    instances.Add(occ12);
-                    instances.Add(occ13);
-                    instances.Add(occ14);
-                    instances.Add(occ15);
-                    instances.Add(occ16);
-                    instances.Add(occ17);
-                    instances.Add(occ18);
-                    instances.Add(occ19);
-                    instances.Add(occ20);
 
-                    Console.WriteLine($"{king} appeared the most in the list");
-                    for (int i = 0; i < instances.Count; i++)
+                    // stores each instance of each element in a list (instances), where the index
+                    // for each element value is element - 10; (i.e. 10s: instances[0], 11s: instances[1]
+                    for (int i = 0; i < randInts.Count - 1; i++)
                     {
-                        Console.Write($"{instances[i]}, ");
+                        instances[randInts[i] - 10]++; // exception: index out of range?
                     }
+                    //for (int i = 0; i < randInts.Count; i++)
+                    //{
+                    //    Console.WriteLine($"{i + 10}: {instances[i]}");
+
+                    //}
+
+
+
+
+
+
+
+                    //// i goes through potential element values
+                    //for (int i = 10; i < 21; i++)
+                    //{
+                    //    // j goes through index numbers
+                    //    for (int j = 0; j < randInts.Count; j++)
+                    //    {
+                    //        //check to see if each element matches i, then goes through a series of if statements to determine what that value is
+                    //        if (randInts[j] == i)
+                    //        {
+                    //            if (i == 10)
+                    //            {
+                    //                // adds to a counter for each element value, these are then compared later
+                    //                occ10++;
+                    //                Console.WriteLine($"i10: {i}");
+                    //                Console.WriteLine($"j10: {j}");
+                    //                // crashes before an element that == i?
+                    //            }
+                    //            else if (i == 11)
+                    //            {
+                    //                occ11++;
+                    //                Console.WriteLine($"i11: {i}");
+                    //                Console.WriteLine($"j11: {j}");
+                    //            }
+                    //            else if (i == 12)
+                    //            {
+                    //                occ12++;
+                    //                Console.WriteLine($"i12: {i}");
+                    //                Console.WriteLine($"j12: {j}");
+                    //            }
+                    //            else if (i == 13)
+                    //            {
+                    //                occ13++;
+                    //                Console.WriteLine($"i13: {i}");
+                    //                Console.WriteLine($"j13: {j}");
+                    //            }
+                    //            else if (i == 14)
+                    //            {
+                    //                occ14++;
+                    //                Console.WriteLine($"i14: {i}");
+                    //                Console.WriteLine($"j14: {j}");
+                    //            }
+                    //            else if (i == 15)
+                    //            {
+                    //                occ15++;
+                    //                Console.WriteLine($"i15: {i}");
+                    //                Console.WriteLine($"j15: {j}");
+                    //            }
+                    //            else if (i == 16)
+                    //            {
+                    //                occ16++;
+                    //                Console.WriteLine($"i16: {i}");
+                    //                Console.WriteLine($"j16: {j}");
+                    //            }
+                    //            else if (i == 17)
+                    //            {
+                    //                occ17++;
+                    //                Console.WriteLine($"i17: {i}");
+                    //                Console.WriteLine($"j17: {j}");
+                    //            }
+                    //            else if (i == 18)
+                    //            {
+                    //                occ18++;
+                    //                Console.WriteLine($"i18: {i}");
+                    //                Console.WriteLine($"j18: {j}");
+                    //            }   
+                    //            else if (i == 19)
+                    //            {
+                    //                occ19++;
+                    //                Console.WriteLine($"i19: {i}");
+                    //                Console.WriteLine($"j19: {j}");
+                    //            }
+                    //            else if (i == 20)
+                    //            {
+                    //                occ20++;
+                    //                Console.WriteLine($"i20: {i}");
+                    //                Console.WriteLine($"j20: {j}");
+                    //            }
+                    //        }
+                    //    }
+                    //}
+                    //king = 10;
+                    //if (occ11 > occ10)
+                    //{
+                    //    king = 11;
+                    //}
+                    //if (occ12 > occ10)
+                    //{
+                    //    king = 12;
+                    //}
+                    //if (occ13 > occ10)
+                    //{
+                    //    king = 13;
+                    //}
+                    //if (occ14 > occ10)
+                    //{
+                    //    king = 14;
+                    //}
+                    //if (occ15 > occ10)
+                    //{
+                    //    king = 15;
+                    //}
+                    //if (occ16 > occ10)
+                    //{
+                    //    king = 16;
+                    //}
+                    //if (occ17 > occ10)
+                    //{
+                    //    king = 17;
+                    //}
+                    //if (occ18 > occ10)
+                    //{
+                    //    king = 18;
+                    //}
+                    //if (occ19 > occ10)
+                    //{
+                    //    king = 19;
+                    //}
+                    //if (occ20 > occ10)
+                    //{
+                    //    king = 20;
+                    //}
+                    //Console.WriteLine($"{king} appeared the most in the list");
                 }
+
                 else if (optionSelect.ToUpper() == "EXIT" || optionSelect == "10")
                 {
                     Main();
                 }
             }
         }
+
+        // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         public static void StrList()
         {
             List<string> vegetables = new List<string>();
